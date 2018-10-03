40879f942e7e69d4b9e7de01b8e6c29ead49ba26
kaleomkar <36242058+kaleomkar@users.noreply.github.com>
2018-10-03 17:45:21 +0530

    Add files via upload
    Annotation Image Overlay Effect using CSS

---
+++ annotation/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+		<meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+        <title>Annotation Overlay Effect with CSS3</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href='https://fonts.googleapis.com/css?family=Alegreya+SC:700,400italic' rel='stylesheet' type='text/css' />
+        <link rel="stylesheet" type="text/css" href="css/demo.css" />
+        <link rel="stylesheet" type="text/css" href="css/style.css" />
+    </head>
+    <body>
+        <div class="container">
+			<section>
+				<div class="ao-item">
+					<div class="ao-preview">
+						<input type="checkbox" id="ao-toggle" class="ao-toggle" name="ao-toggle" />
+						<img src="images/Anariel_BlentonTemplate.jpg" alt="image01" />
+						<div class="ao-annotations">
+							<span>Full Localisation Support</span>
+							<span>Custom Image Widget</span>
+							<span>Blog and Contact Widgets</span>
+							<span>Easy Theme Options</span>
+							<span>4 Footer Widget Columns</span>
+						</div>
+					</div>
+				</div>
+				
+				<div class="ao-item ao-item-left">
+					<div class="ao-details">
+						<h2>Blenton is elegant</h2>
+						<p>The design of Blenton makes a great match with a portfolio. It comes with many features like full localisation support and easy theme options.</p>
+						<p><a href="http://www.anarieldesign.com/?gallery=blenton-site-template">Buy the theme &rarr;</a></p>
+					</div>
+					<div class="ao-preview">
+						<input type="checkbox" id="ao-toggle" class="ao-toggle" name="ao-toggle" />
+						<img src="images/Anariel_UnixTemplate.jpg" alt="image02" />
+						<div class="ao-annotations">
+							<span>Easy Logo Upload</span>
+							<span>Custom Post Portfolio</span>
+							<span>Custom Background Color and Pattern</span>
+						</div>
+					</div>
+				</div>
+			</section>
+			
+        </div>
+<script src="js/demoad.js"></script> 
+	</body>
+</html>
