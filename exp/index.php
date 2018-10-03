8039d65e999b93078b64e139272177e2845cadb2
kaleomkar <36242058+kaleomkar@users.noreply.github.com>
2018-10-03 17:48:41 +0530

    Add files via upload

---
+++ exp/index.php
@@ -0,0 +1,150 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta charset="utf-8" />
+	
+	<link type="text/css" media="all" href="css/bootstrap.min.css" rel="stylesheet" />
+	<link type="text/css" media="all" href="css/breeze1.css" rel="stylesheet" />
+	<!----<link type="text/css" media="all" href="css/breeze2.css" rel="stylesheet" />---->
+	<!----<link type="text/css" media="all" href="css/breeze3.css" rel="stylesheet" />----->
+	<!----<link type="text/css" media="all" href="css/breeze4.css" rel="stylesheet" />---->
+	<link type="text/css" media="all" href="css/breeze5.css" rel="stylesheet" />
+	<!-----<link type="text/css" media="all" href="css/breeze6.css" rel="stylesheet" />------>
+	<!---<link type="text/css" media="all" href="css/breeze7.css" rel="stylesheet" />---->
+	<link type="text/css" media="all" href="css/breeze8.css" rel="stylesheet" />
+	<link type="text/css" media="all" href="css/aos.css" rel="stylesheet" />
+	
+	<!------<link type="text/css" media="all" href="css/breeze9.css" rel="stylesheet" />----->
+	<!-----<link type="text/css" media="all" href="css/breeze10.css" rel="stylesheet" />---->
+	
+	<script src="js/jquery.js"></script>
+	<script type="text/javascript">
+		AOS.init();
+	</script>
+	<script type="text/javascript" src="js/aos.js"></script>
+
+	<title>Book Store</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<link rel='dns-prefetch' href='//fonts.googleapis.com' />
+
+	<!-----<link rel='stylesheet' id='style-custom-css' href='css/customstyle.css' type='text/css' media='all' />----->
+	<link rel='stylesheet' id='Google-Font-css' href='http://fonts.googleapis.com/css?family=Droid+Serif%3Asubset%3Dlatin%3An%2Ci%2Cb%2Cbi%7C&#038;ver=4.9.8' type='text/css' media='all' />
+	<link rel='stylesheet' id='ls-google-fonts-css' href='http://fonts.googleapis.com/css?family=Lato:100,300,regular,700,900%7COpen+Sans:300%7CIndie+Flower:regular%7COswald:300,regular,700&#038;subset=latin%2Clatin-ext' type='text/css' media='all' />
+	
+	<link rel="stylesheet" href="css/font-awesome.min.css" />
+	<script type="text/javascript" src="js/breeze1.js"></script>
+	<script type="text/javascript" src="js/breeze2.js"></script>
+	<script type="text/javascript" src="js/breeze3.js"></script>
+	<script type="text/javascript" src="js/breeze4.js"></script>
+	<script type="text/javascript" src="js/breeze5.js"></script>
+	<script type="text/javascript" src="js/breeze6.js"></script>
+	<script type="text/javascript" src="js/breeze7.js"></script>
+	<script type="text/javascript" src="js/breeze8.js"></script>
+	<script type="text/javascript" src="js/breeze9.js"></script>
+	<script type="text/javascript" src="js/breeze10.js"></script>
+	<script type="text/javascript" src="js/breeze11.js"></script>
+	<script type="text/javascript" src="js/breeze12.js"></script>
+	<script type="text/javascript" src="js/breeze13.js"></script>
+	<script type="text/javascript" src="js/breeze14.js"></script>
+	<script type="text/javascript" src="js/breeze15.js"></script>
+	<script type="text/javascript" src="js/breeze16.js"></script>	
+</head>
+
+<body>
+	<div class="boxFullWidth" style="margin-bottom:0;">
+		<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a href="" class="navbar-brand" style="margin-top:-10px;">LOGO</a>
+    </div>
+
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="page1.php">Page 1</a></li>
+        <li><a href="">Page 2</a></li>
+		<li><a href="">Page 3</a></li>
+		<li><a href="">Page 4</a></li>
+		<li><a href="">Page 5</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+	</div>
+	
+	<div class="wrapper">		
+		<div class="slider-wrapper">
+			<section class="slider-container">
+				<div id="layerslider_2" class="ls-wp-container fitvidsignore" style="width:1900px;height:590px;margin:0 auto;margin-bottom: 0px;">
+					<div class="ls-slide" data-ls="duration:8000;kenburnsscale:1.2;">
+						<img width="1900" height="590" src="images/ok/slide-bg-11.jpg" class="ls-bg" alt="" />
+						<img width="702" height="566" src="images/ok/slide-img-1.png" class="ls-l" alt="" style="top:25px;left:832px;" data-ls="offsetxin:right;durationin:2000;fadein:false;offsetxout:left;durationout:1500;fadeout:false;parallaxlevel:0;" />
+						<img width="202" height="224" src="http://bookstore.crunchpress.com/wp-content/uploads/2017/06/slide-img-3.png" class="ls-l" alt="" style="top:277px;left:366px;" data-ls="offsetyin:top;durationin:2000;delayin:250;fadein:false;offsetxout:right;durationout:1500;fadeout:false;parallaxlevel:0;" />
+						<img width="177" height="131" src="http://bookstore.crunchpress.com/wp-content/uploads/2017/06/slide-img-5.png" class="ls-l" alt="" style="top:374px;left:575px;" data-ls="offsetyin:top;durationin:2300;delayin:300;fadein:false;offsetxout:right;durationout:1500;fadeout:false;parallaxlevel:0;" />
+						<img width="423" height="69" src="http://bookstore.crunchpress.com/wp-content/uploads/2017/06/slide-img-4.png" class="ls-l" alt="" srcset="http://bookstore.crunchpress.com/wp-content/uploads/2017/06/slide-img-4.png 423w, http://bookstore.crunchpress.com/wp-content/uploads/2017/06/slide-img-4-300x49.png 300w" sizes="(max-width: 423px) 100vw, 423px" style="top:175px;left:366px;" data-ls="offsetxin:left;durationin:2500;delayin:200;fadein:false;offsetxout:right;durationout:1500;fadeout:false;parallaxlevel:0;" /><img width="423" height="55" src="http://bookstore.crunchpress.com/wp-content/uploads/2017/06/slide-img-2.png" class="ls-l" alt="" srcset="http://bookstore.crunchpress.com/wp-content/uploads/2017/06/slide-img-2.png 423w, http://bookstore.crunchpress.com/wp-content/uploads/2017/06/slide-img-2-300x39.png 300w" sizes="(max-width: 423px) 100vw, 423px" style="top:103px;left:364px;" data-ls="offsetxin:left;durationin:2800;delayin:100;fadein:false;offsetxout:right;durationout:1500;fadeout:false;parallaxlevel:0;" />
+					</div>
+					<div class="ls-slide" data-ls="duration:8000;kenburnsscale:1.2;">
+						<img width="1900" height="590" src="images/ok/slide2-bg-2.jpg" class="ls-bg" alt=""  />
+						<img width="160" height="131" src="http://bookstore.crunchpress.com/wp-content/uploads/2017/06/slide2-img-6.png" class="ls-l" alt="" style="top:25px;left:864px;" data-ls="offsetyin:bottom;durationin:1700;delayin:450;fadein:false;offsetyout:top;durationout:1700;fadeout:false;parallaxlevel:0;" />
+						<img width="450" height="312" src="images/ok/truck.png" class="ls-l" alt="" style="top:200px;left:100px;" data-ls="offsetxin:right;durationin:4500;delayin:400;fadein:false;offsetxout:left;durationout:4500;fadeout:false;parallaxlevel:0;" />
+						<img width="369" height="311" src="images/ok/screen.png" class="ls-l" alt="" style="top:160px;left:764px;" data-ls="offsetxin:right;durationin:3500;delayin:250;fadein:false;offsetxout:right;durationout:3500;fadeout:false;parallaxlevel:0;" />
+						<img width="182" height="319" src="http://bookstore.crunchpress.com/wp-content/uploads/2017/06/slide2-img-7.png" class="ls-l" alt="" srcset="http://bookstore.crunchpress.com/wp-content/uploads/2017/06/slide2-img-7.png 182w, http://bookstore.crunchpress.com/wp-content/uploads/2017/06/slide2-img-7-171x300.png 171w" sizes="(max-width: 182px) 100vw, 182px" style="top:113px;left:1163px;" data-ls="offsetxin:left;durationin:3500;delayin:300;fadein:false;offsetxout:right;durationout:3500;fadeout:false;parallaxlevel:0;" />
+						<img width="748" height="68" src="http://bookstore.crunchpress.com/wp-content/uploads/2017/06/slide2-img-3.png" class="ls-l" alt="" srcset="http://bookstore.crunchpress.com/wp-content/uploads/2017/06/slide2-img-3.png 748w, http://bookstore.crunchpress.com/wp-content/uploads/2017/06/slide2-img-3-300x27.png 300w" sizes="(max-width: 748px) 100vw, 748px" style="top:479px;left:572px;" data-ls="offsetyin:top;durationin:4000;delayin:500;fadein:false;offsetxout:right;durationout:4000;fadeout:false;parallaxlevel:0;" />
+						<img width="182" height="319" src="http://bookstore.crunchpress.com/wp-content/uploads/2017/06/slide2-img-5.png" class="ls-l" alt="" srcset="http://bookstore.crunchpress.com/wp-content/uploads/2017/06/slide2-img-5.png 182w, http://bookstore.crunchpress.com/wp-content/uploads/2017/06/slide2-img-5-171x300.png 171w" sizes="(max-width: 182px) 100vw, 182px" style="top:113px;left:552px;" data-ls="offsetxin:right;durationin:3500;delayin:400;offsetxout:left;durationout:3500;parallaxlevel:0;" />
+						<img width="187" height="510" src="images/slide2-img-2.png" class="ls-l" alt="" srcset="http://bookstore.crunchpress.com/wp-content/uploads/2017/06/slide2-img-2.png 187w, http://bookstore.crunchpress.com/wp-content/uploads/2017/06/slide2-img-2-110x300.png 110w" sizes="(max-width: 187px) 100vw, 187px" style="top:56px;left:1349px;" data-ls="offsetxin:left;durationin:4500;delayin:300;offsetxout:right;durationout:4500;parallaxlevel:0;" />
+						<img width="182" height="319" src="http://bookstore.crunchpress.com/wp-content/uploads/2017/06/slide2-img-5.png" class="ls-l" alt="" srcset="http://bookstore.crunchpress.com/wp-content/uploads/2017/06/slide2-img-5.png 182w, http://bookstore.crunchpress.com/wp-content/uploads/2017/06/slide2-img-5-171x300.png 171w" sizes="(max-width: 182px) 100vw, 182px" style="top:113px;left:552px;" data-ls="offsetxin:right;durationin:3500;delayin:200;offsetxout:left;durationout:3500;parallaxlevel:0;" />
+					</div>
+					
+					<div class="ls-slide" data-ls="duration:8000;kenburnsscale:1.2;">
+						<img width="1900" height="590" src="http://bookstore.crunchpress.com/wp-content/uploads/2017/06/slide-bg-3.png" class="ls-bg" alt="" srcset="http://bookstore.crunchpress.com/wp-content/uploads/2017/06/slide-bg-3.png 1900w, http://bookstore.crunchpress.com/wp-content/uploads/2017/06/slide-bg-3-300x93.png 300w, http://bookstore.crunchpress.com/wp-content/uploads/2017/06/slide-bg-3-768x238.png 768w, http://bookstore.crunchpress.com/wp-content/uploads/2017/06/slide-bg-3-1024x318.png 1024w" sizes="(max-width: 1900px) 100vw, 1900px" />
+						<img width="220" height="66" src="http://bookstore.crunchpress.com/wp-content/uploads/2017/06/slide3-img-2.png" class="ls-l" alt="" style="top:178px;left:739px;" data-ls="offsetyin:top;durationin:3200;delayin:800;fadein:false;offsetxout:right;durationout:3200;fadeout:false;parallaxlevel:0;" />
+						<img width="500" height="279" src="http://bookstore.crunchpress.com/wp-content/uploads/2017/06/slide3-img-1.png" class="ls-l" alt="" srcset="http://bookstore.crunchpress.com/wp-content/uploads/2017/06/slide3-img-1.png 500w, http://bookstore.crunchpress.com/wp-content/uploads/2017/06/slide3-img-1-300x167.png 300w" sizes="(max-width: 500px) 100vw, 500px" style="top:183px;left:454px;" data-ls="offsetxin:right;durationin:4000;delayin:700;fadein:false;offsetxout:right;durationout:4000;fadeout:false;parallaxlevel:0;" />
+						<img width="593" height="77" src="http://bookstore.crunchpress.com/wp-content/uploads/2017/06/slide3-img-3.png" class="ls-l" alt="" srcset="http://bookstore.crunchpress.com/wp-content/uploads/2017/06/slide3-img-3.png 593w, http://bookstore.crunchpress.com/wp-content/uploads/2017/06/slide3-img-3-300x39.png 300w" sizes="(max-width: 593px) 100vw, 593px" style="top:243px;left:834px;" data-ls="offsetxin:right;durationin:4500;delayin:600;fadein:false;offsetxout:right;durationout:4500;fadeout:false;parallaxlevel:0;" />
+						<img width="593" height="105" src="http://bookstore.crunchpress.com/wp-content/uploads/2017/06/slide3-img-4.png" class="ls-l" alt="" srcset="http://bookstore.crunchpress.com/wp-content/uploads/2017/06/slide3-img-4.png 593w, http://bookstore.crunchpress.com/wp-content/uploads/2017/06/slide3-img-4-300x53.png 300w" sizes="(max-width: 593px) 100vw, 593px" style="top:320px;left:833px;" data-ls="durationin:5000;delayin:500;durationout:5000;parallaxlevel:0;" />
+					</div>
+				</div>
+			</section>
+		</div>
+	</div>
+	
+	
+	
+	<div class="boxFullWidth padding-tb50 text-center">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="col-lg-3">
+						<h1>Block 1</h1>
+						<p>On July 16, 1969, the Apollo 11 spacecraft launched from the Kennedy Space Center in Florida. Its mission was to go where no human being had gone before—the moon! The crew consisted of Neil Armstrong, Michael Collins, and Buzz Aldrin. The spacecraft landed on the moon in the Sea of Tranquility, a basaltic flood plain, on July 20, 1969. The moonwalk took place the following day. On July 21, 1969, at precisely 10:56 EDT, Commander Neil Armstrong emerged from the Lunar Module and took his famous first step onto the moon’s surface. He declared, “That’s one small step for man, one giant leap for mankind.” It was a monumental moment in human history!</p>
+					</div>
+					<div class="col-lg-3" data-aos="fade-down">
+						<h1>Block 2</h1>
+						<p>On July 16, 1969, the Apollo 11 spacecraft launched from the Kennedy Space Center in Florida. Its mission was to go where no human being had gone before—the moon! The crew consisted of Neil Armstrong, Michael Collins, and Buzz Aldrin. The spacecraft landed on the moon in the Sea of Tranquility, a basaltic flood plain, on July 20, 1969. The moonwalk took place the following day. On July 21, 1969, at precisely 10:56 EDT, Commander Neil Armstrong emerged from the Lunar Module and took his famous first step onto the moon’s surface. He declared, “That’s one small step for man, one giant leap for mankind.” It was a monumental moment in human history!</p>
+					</div>
+					<div class="col-lg-3" data-aos="fade-down">
+						<h1>Block 3</h1>
+						<p>On July 16, 1969, the Apollo 11 spacecraft launched from the Kennedy Space Center in Florida. Its mission was to go where no human being had gone before—the moon! The crew consisted of Neil Armstrong, Michael Collins, and Buzz Aldrin. The spacecraft landed on the moon in the Sea of Tranquility, a basaltic flood plain, on July 20, 1969. The moonwalk took place the following day. On July 21, 1969, at precisely 10:56 EDT, Commander Neil Armstrong emerged from the Lunar Module and took his famous first step onto the moon’s surface. He declared, “That’s one small step for man, one giant leap for mankind.” It was a monumental moment in human history!</p>
+					</div>
+					<div class="col-lg-3" data-aos="fade-down">
+						<h1>Block 4</h1>
+						<p>On July 16, 1969, the Apollo 11 spacecraft launched from the Kennedy Space Center in Florida. Its mission was to go where no human being had gone before—the moon! The crew consisted of Neil Armstrong, Michael Collins, and Buzz Aldrin. The spacecraft landed on the moon in the Sea of Tranquility, a basaltic flood plain, on July 20, 1969. The moonwalk took place the following day. On July 21, 1969, at precisely 10:56 EDT, Commander Neil Armstrong emerged from the Lunar Module and took his famous first step onto the moon’s surface. He declared, “That’s one small step for man, one giant leap for mankind.” It was a monumental moment in human history!</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	
+	
+	<script type="text/javascript" src="http://bookstore.crunchpress.com/wp-content/cache/breeze-minification/js/breeze_f31ecb3d214f32fcfe19ef82cc7fe242.js"></script>
+	<script type="text/javascript" src="http://bookstore.crunchpress.com/wp-content/cache/breeze-minification/js/breeze_be2f1d131d7fd87fcfaa53bc2ea46f30.js"></script>
+	
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ exp/page1.php
@@ -0,0 +1,74 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta charset="utf-8" />
+
+	<link type="text/css" media="all" href="css/bootstrap.min.css" rel="stylesheet" />
+	<link type="text/css" media="all" href="css/breeze1.css" rel="stylesheet" />
+	<link type="text/css" media="all" href="css/breeze5.css" rel="stylesheet" />
+	<link type="text/css" media="all" href="css/breeze8.css" rel="stylesheet" />
+	
+	<script src="js/jquery.js"></script>
+
+	<title>Book Store</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<link rel='dns-prefetch' href='//fonts.googleapis.com' />
+	
+	<link rel='stylesheet' id='Google-Font-css' href='http://fonts.googleapis.com/css?family=Droid+Serif%3Asubset%3Dlatin%3An%2Ci%2Cb%2Cbi%7C&#038;ver=4.9.8' type='text/css' media='all' />
+	<link rel='stylesheet' id='ls-google-fonts-css' href='http://fonts.googleapis.com/css?family=Lato:100,300,regular,700,900%7COpen+Sans:300%7CIndie+Flower:regular%7COswald:300,regular,700&#038;subset=latin%2Clatin-ext' type='text/css' media='all' />
+	
+	<link rel="stylesheet" href="css/font-awesome.min.css" />
+	<script type="text/javascript" src="js/breeze1.js"></script>
+	<script type="text/javascript" src="js/breeze2.js"></script>
+	<script type="text/javascript" src="js/breeze3.js"></script>
+	<script type="text/javascript" src="js/breeze4.js"></script>
+	<script type="text/javascript" src="js/breeze5.js"></script>
+	<script type="text/javascript" src="js/breeze6.js"></script>
+	<script type="text/javascript" src="js/breeze7.js"></script>
+	<script type="text/javascript" src="js/breeze8.js"></script>
+	<script type="text/javascript" src="js/breeze9.js"></script>
+	<script type="text/javascript" src="js/breeze10.js"></script>
+	<script type="text/javascript" src="js/breeze11.js"></script>
+	<script type="text/javascript" src="js/breeze12.js"></script>
+	<script type="text/javascript" src="js/breeze13.js"></script>
+	<script type="text/javascript" src="js/breeze14.js"></script>
+	<script type="text/javascript" src="js/breeze15.js"></script>
+	<script type="text/javascript" src="js/breeze16.js"></script>	
+</head>
+
+<body>
+	<div class="boxFullWidth" style="margin-bottom:0;">
+		<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a href="" class="navbar-brand" style="margin-top:-10px;">LOGO</a>
+    </div>
+
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="page1.php">Page 1</a></li>
+        <li><a href="">Page 2</a></li>
+		<li><a href="">Page 3</a></li>
+		<li><a href="">Page 4</a></li>
+		<li><a href="">Page 5</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+	</div>
+	
+	
+	
+	
+	<script type="text/javascript" src="http://bookstore.crunchpress.com/wp-content/cache/breeze-minification/js/breeze_f31ecb3d214f32fcfe19ef82cc7fe242.js"></script>
+	<script type="text/javascript" src="http://bookstore.crunchpress.com/wp-content/cache/breeze-minification/js/breeze_be2f1d131d7fd87fcfaa53bc2ea46f30.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+</body>
+</html>
